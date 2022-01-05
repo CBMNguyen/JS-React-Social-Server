9f505bbd0a5ab2f4cd7f0b0c612d777f769dcd3b
Hieu Nguyen <hieub1809236@student.ctu.edu.vn>
2022-01-05 09:09:31 +0700

    add all route in index file

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 2022;
 dotenv.config();
 
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const conversationRoute = require("./routes/conversations");
+const messageRoute = require("./routes/messages");
+
 mongoose.connect(process.env.MONGODB_URL, () =>
   console.log("Connected to MongoDB...")
 );
@@ -17,6 +23,12 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(morgan("dev"));
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
 
 // Test and Throw error
 
